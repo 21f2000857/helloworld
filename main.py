8973b317f40da2bc3c310c51d6e8ca13a4a2f0c9
21f2000857 <153115679+21f2000857@users.noreply.github.com>
2023-12-06 19:09:09 +0000

    move main content to Hello.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.write("# Welcome to Streamlit! 👋")
-num1 = st.number_input('num 1')
-num2 = st.number_input('num 2')
-num3 = st.number_input('num 3')
-
-max_num = max(num1, num2, num3)
-st.write(f"max number is {max_num}")
